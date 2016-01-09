c9c1d216fa8e8d5cc43e0c3789e7586b03087afa
yaknastak <yaknastak@gmail.com>
2016-01-09 15:26:11 +0300

    'dz2'

---
+++ Yakovleva_dz2_amch.py
@@ -0,0 +1,41 @@
+﻿def converter (text, alpha1, alpha2):
+    i = len(alpha1) - 1
+    while i >= 0:
+        text = text.replace(alpha1[i], alpha2[i])
+        i -= 1
+    return text
+
+f1 = open('alpha_amch.csv', 'r', encoding = 'utf-8')
+f2 = open('new', 'w', encoding = 'utf-8')
+inp = input("Введите имя файла, содержащего текст на амхарском языке: ")
+f3 = open(inp, 'r', encoding = 'utf-8')
+voc = []
+symbols = []
+syllables = []
+horizontal = 0
+d ={}
+lst = []
+for line in f1:
+    line = line.strip('\n')
+    line = line.split('\t')
+    for letter in line:
+        if horizontal == 0 and letter != line[0]:
+            voc.append(letter)
+        elif horizontal > 0 and letter == line[0]:
+                b = 0
+                while b < len(voc):
+                    syl = letter + voc[b]
+                    d[line[b+1]] = syl
+                    b += 1
+    horizontal += 1
+    
+for (key, val) in d.items():
+    symbols.append(key)
+    syllables.append(val)
+
+for line in f3:
+    line = line.lower()
+    f2.write(converter(line, symbols, syllables))
+f1.close()
+f2.close()
+f3.close()
---
+++ Yakovleva_dz2_irish.py
@@ -0,0 +1,20 @@
+﻿import re
+import string
+
+inp = input("Введите имя файла: ")
+f1 = open(inp, 'r', encoding = 'utf-8')
+f2 = open('heads', 'w', encoding = 'utf-8')
+rgexp = '<h3 headword_id="[0-9]+">([a-zA-Z\-\']+)'
+rgexp2 = '(.*)</p></div><div class="col-sm-9 result_content">'
+for line in f1:
+    m = re.search(rgexp, line)
+    m2 = re.search(rgexp2, line)
+    if m != None:
+        f2.write("Headword: " + m.group(1) + '\n')
+    if m2 != None:
+        forms = m2.group(1)
+        forms = forms.strip()
+        f2.write('Forms: ' + forms)
+            
+f1.close()
+f2.close()
