5b232034c809251d8c99482be51f93df8a9d64fc
Anastasia Yakovleva <yaknastak@gmail.com>
2016-06-20 05:50:45 +0300

    Update my_sql.py

--- sql/my_sql.py
+++ sql/my_sql.py
@@ -6,27 +6,27 @@ def main():
     dbname = 'guest1_YAKOVLEVA_VK'
     conn = pymysql.connect(host='localhost', user='guest1',db = 'guest1_vk', passwd ='n76Je4=wx6H', charset = 'utf8')
     cur = conn.cursor()
-    sql = 'create database ' + dbname + ' DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;'
+    sql = 'CREATE DATABASE ' + dbname + ' DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;'
     try:
         cur.execute(sql)
     except: 
         print(Exception)
     cur.execute('use ' + dbname + ';')
-    try: cur.execute('create table metadata (id INT, sex INT(2), PRIMARY KEY(id));')
+    try: cur.execute('CREATE TABLE metadata (id INT, sex INT(2), PRIMARY KEY(id));')
     except: print('table already exists')
     with open('meta.csv', encoding = 'utf-8') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=',')
         for row in spamreader:
             db_row = "'" + str(row['uid']) + "','" + str(row['sex']) + "'"
-            cur.execute('insert into metadata (sthm id, sex) value(' + db_row + ');')
-    try: cur.execute('create table walls (id INT, date INT, text VARCHAR(20000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')
+            cur.execute('INSERT INTO metadata (id, sex) VALUE(' + db_row + ');')
+    try: cur.execute('CREATE TABLE walls (id INT, date INT, text VARCHAR(20000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')
     except: print('table already exists')
     with open('tara.csv', newline='', encoding = 'utf-8') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=',')
         for row in spamreader:
             db_row1 = "'" + str(row['id']) + "','" + str(row['date']) + "','" + row['post'] + "'"
             try:
-                cur.execute('insert into walls (id, date, text) value(' + db_row1 + ');')
+                cur.execute('INSERT INTO walls (id, date, text) value(' + db_row1 + ');')
             except:
                 print(row['post'])
             
