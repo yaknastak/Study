ac8be483ed01cddb9816a44bcd7cabe0516aa9f7
Anastasia Yakovleva <yaknastak@gmail.com>
2016-06-20 06:01:52 +0300

    Update my_sql.py

--- sql/my_sql.py
+++ sql/my_sql.py
@@ -12,21 +12,21 @@ def main():
     except: 
         print(Exception)
     cur.execute('use ' + dbname + ';')
-    try: cur.execute('CREATE TABLE metadata (id INT, sex INT(2), PRIMARY KEY(id));')
+    try: cur.execute('CREATE TABLE `metadata` (`id` INT, `sex` INT(2), PRIMARY KEY(id));')
     except: print('table already exists')
     with open('meta.csv', encoding = 'utf-8') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=',')
         for row in spamreader:
             db_row = "'" + str(row['uid']) + "','" + str(row['sex']) + "'"
-            cur.execute('INSERT INTO metadata (id, sex) VALUE(' + db_row + ');')
-    try: cur.execute('CREATE TABLE walls (id INT, date INT, text VARCHAR(20000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')
+            cur.execute('INSERT INTO `metadata` (`id`, `sex`)VALUES (' + db_row + ');')
+    try: cur.execute('CREATE TABLE `walls` (`id` INT, `date` INT, `text` VARCHAR(10000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')
     except: print('table already exists')
     with open('tara.csv', newline='', encoding = 'utf-8') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=',')
         for row in spamreader:
             db_row1 = "'" + str(row['id']) + "','" + str(row['date']) + "','" + row['post'] + "'"
             try:
-                cur.execute('INSERT INTO walls (id, date, text) value(' + db_row1 + ');')
+                cur.execute('INSERT INTO `walls` (`id`, `date`, `text`)VALUES (' + db_row1 + ');')
             except:
                 print(row['post'])
             
