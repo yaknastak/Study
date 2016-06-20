fc72470a8e3aaac0f1f17bc8de45af83ba561216
Anastasia Yakovleva <yaknastak@gmail.com>
2016-06-20 05:43:40 +0300

    Update my_sql.py

--- sql/my_sql.py
+++ sql/my_sql.py
@@ -12,23 +12,19 @@ def main():
     except: 
         print(Exception)
     cur.execute('use ' + dbname + ';')
-    try:
-        cur.execute('create table metadata (sth NULL, id INT, sex INT(2), PRIMARY KEY(sth));')
-    except: 
-        print(Exception)
-    with open('meta.csv', newline='', encoding = 'utf-8') as csvfile:
+    try: cur.execute('create table metadata (id INT, sex INT(2), PRIMARY KEY(id));')
+    except: print('table already exists')
+    with open('meta.csv', encoding = 'utf-8') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=',')
         for row in spamreader:
-            db_row = str(row['uid']) + ',' + str(row['sex'])
-            cur.execute('insert into metadata (sthm id, sex) value(null, ' + db_row + ');')
-    try:
-        cur.execute('create table walls (id INT, date INT, text VARCHAR(20000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')
-    except: 
-        print(Exception)
+            db_row = "'" + str(row['uid']) + "','" + str(row['sex']) + "'"
+            cur.execute('insert into metadata (sthm id, sex) value(' + db_row + ');')
+    try: cur.execute('create table walls (id INT, date INT, text VARCHAR(20000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')
+    except: print('table already exists')
     with open('tara.csv', newline='', encoding = 'utf-8') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=',')
         for row in spamreader:
-            db_row1 = "'" + str(row['id']) + ',' + str(row['date']) + "','" + row['post'] + "'"
+            db_row1 = "'" + str(row['id']) + "','" + str(row['date']) + "','" + row['post'] + "'"
             try:
                 cur.execute('insert into walls (id, date, text) value(' + db_row1 + ');')
             except:
