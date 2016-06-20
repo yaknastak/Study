ded36185efe3318a1f9bbd45cb3a2730fa5010a4
Anastasia Yakovleva <yaknastak@gmail.com>
2016-06-20 07:02:27 +0300

    Update my_sql.py

--- sql/my_sql.py
+++ sql/my_sql.py
@@ -17,13 +17,13 @@ def main():
     try: cur.execute('CREATE TABLE `walls` (`id` INT, `date` INT, `text` VARCHAR(10000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')
     except: print('table already exists')
     with open('tara.csv', newline='', encoding = 'utf-8') as csvfile:
-    spamreader = csv.DictReader(csvfile, delimiter=',')
-    for row in spamreader:
-        db_row1 = "'" + str(row['id']) + "','" + str(row['date']) + "','" + row['post'] + "'"
-        try:
-            cur.execute('INSERT INTO `walls` (`id`, `date`, `text`)VALUES (' + db_row1 + ');')
-        except:
-            print(row['post'])
+        spamreader = csv.DictReader(csvfile, delimiter=',')
+        for row in spamreader:
+            db_row1 = "'" + str(row['id']) + "','" + str(row['date']) + "','" + row['post'] + "'"
+            try:
+                cur.execute('INSERT INTO `walls` (`id`, `date`, `text`)VALUES (' + db_row1 + ');')
+            except:
+                print(row['post'])
     conn.commit()
     cur.close()
     conn.close()
