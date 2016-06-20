1b49fe3aa7c11556c52c8688d0fe3a47f56cbe15
Anastasia Yakovleva <yaknastak@gmail.com>
2016-06-20 06:41:29 +0300

    Update my_sql.py

--- sql/my_sql.py
+++ sql/my_sql.py
@@ -13,8 +13,8 @@ def main():
         spamreader = csv.DictReader(csvfile, delimiter=',')
         for row in spamreader:
             db_row = "'" + str(row['uid']) + "','" + str(row['sex']) + "'"
-            cur.execute('INSERT INTO `metadata` (`id`, `sex`)VALUES (' + db_row + ');')
-
+           # cur.execute('INSERT INTO `metadata` (`id`, `sex`)VALUES (' + db_row + ');')
+             cur.execute('INSERT INTO `metadata` (`id`, `sex`)VALUES (' + "'1345'" + ',' + "'2'" + ');')
     conn.commit()
     cur.close()
     conn.close()
